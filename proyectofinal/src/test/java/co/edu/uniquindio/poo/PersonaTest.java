eade382e29f19cfc2d4e8a9daf930ba37faac2f2
Juan Esteban Carmona Salazar <juanes.carmonas@uqvirtual.edu.co>
2024-05-26 16:18:43 -0500

    Pruebas
    Se agrega prueba de la clase Persona

--- proyectofinal/src/test/java/co/edu/uniquindio/poo/PersonaTest.java
+++ proyectofinal/src/test/java/co/edu/uniquindio/poo/PersonaTest.java
@@ -1,5 +1,32 @@
 package co.edu.uniquindio.poo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
 public class PersonaTest {
+    private static Logger LOG = Logger.getLogger(PersonaTest.class.getName());
+
+    /*
+     * Prueba para verificar que un usuario no tenga datos nulos
+     */
+    @Test
+    public void datosNulos() {
+        LOG.info("Inicio test datosNulos");
+        Persona persona = new Persona("Juan", "Carmona", "1094878402");
+        assertThrows(Throwable.class, () -> new Persona(null, null, null));
+        LOG.info("Finalización test datosNulos");
+    }
+
+    /*
+     * Test para verificar que los datos no esten vacios
+     */
+    @Test
+    public void datosVacios() {
+        LOG.info("Iniciando test datosVacios");
+
+        assertThrows(Throwable.class, () -> new Persona(" ", " ", " "));
+        LOG.info("Finalizando test datosVacios");
+    }
 
 }
